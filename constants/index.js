5ce67011e9276ca8066bee01eac9729f4fc84eee
Zaggi2021 <107500694+Zaggi2021@users.noreply.github.com>
2023-07-28 02:48:20 +0300

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x427c14a8080ea07692778865dbbf0c1ff3ec79cc";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
